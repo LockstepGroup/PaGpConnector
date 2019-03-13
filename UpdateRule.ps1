36878a9204280a9d4549a26a3f1bee5d6268ff45
Brian Addicks <brian@addicks.us>
2019-03-12 21:15:48 -0400

    first pass at UpdateRule apie

---
+++ UpdateRule.ps1
@@ -0,0 +1,217 @@
+#Requires -Modules Corkscrew,UniversalDashboard
+
+$ApiName = "updaterule"
+
+$EndpointInitialization = New-UDEndpointInitialization -Variable RootPath
+
+$Endpoint = New-UDEndpoint -Url "/updaterule" -Method "POST" -Endpoint {
+    param($Body)
+
+    trap {
+        $TroubleshootingData.Error += $_
+    }
+
+    $TroubleshootingData = @{}
+    $TroubleshootingData.Error = @()
+
+    # Used to measure run times
+    $StopWatch = [system.diagnostics.stopwatch]::StartNew()
+
+    # Import Modules
+    Import-Module CorkScrew
+    Import-Module PowerAlto
+
+    # $MyInvocation doesn't appear to exist for Endpoints, gotta set it manually
+    $RootPath = '/Users/brian/dev/PaGpConnector'
+
+    $TsFile = Join-Path -Path $RootPath -ChildPath "ts.xml"
+    $ConfigPath = Join-Path -Path $RootPath -ChildPath "config.json"
+
+    # Setup Logging
+    $Global:LogFile = Join-Path -Path $RootPath -ChildPath 'updaterule.log' #TODO: pull from config
+    $global:LogThreshold = 5 #TODO: pull from config
+
+    log 1 "Starting updaterule" -LogHeader
+
+    # Import Configuration
+    $Config = Get-CsConfiguration $ConfigPath
+
+    # Create a credential to decode apikey
+    $ApiKey = ConvertTo-SecureString $Config.ApiKey -Key $Config.AesKey
+    $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList 'user', $ApiKey
+    $ApiKey = $Credential.GetNetworkCredential().Password
+
+    try {
+        $Connect = Get-PaDevice -DeviceAddress $Config.PaDevice -ApiKey $ApiKey
+    } catch {
+        #TODO: needs some error handling
+    }
+
+    # These tags are a minimum of what will be needed.
+    $NeededTags = @()
+    $NeededTags += 'vendor'
+    $NeededTags += 'cpe'
+
+    try {
+        $GpEventRx = [regex] "GlobalProtect\ gateway\ client\ configuration\ (?<action>generated|released).\ User\ name:\ (?<user>.+?),"
+        $GpMatch = $GpEventRx.Match($Body)
+        if ($GpMatch.Success) {
+            # Extract what we need from the log message
+            $Action = $GpMatch.Groups['action'].Value
+            $User = $GpMatch.Groups['user'].Value
+
+            # get just the user part of an email address to use as a tag for the vendor
+            $NewTagRx = [regex] "(.+)(?=@)?"
+            $UserTag = $NewTagRx.Match($User).Groups[0].Value
+
+            # Get devices from api
+            $Rest = @{}
+            $Rest.Uri = "http://localhost:10005/api/getdevice/$UserTag"
+            $Response = Invoke-RestMethod @Rest
+            $DeviceList = $Response
+
+            # Get Address list from PA, may need to narrow this down later, but I didn't want to do multiple API calls.
+            $CurrentAddresses = Get-PaAddress
+
+            # Setup names for the new Group and Policy
+            $GroupName = 'cpegrp_' + $UserTag
+            $PolName = 'cpe_' + $UserTag
+
+            switch ($Action) {
+                'generated' {
+                    log 2 "Creating rules for user $User"
+
+                    ###################################
+                    # Add Tags
+                    $NeededTags += $UserTag
+                    $CurrentTags = Get-PaTag
+                    foreach ($tag in $NeededTags) {
+                        $Lookup = $CurrentTags | Where-Object { $_.Name -eq $UserTag }
+                        if (!($Lookup)) {
+                            $CreateTag = New-PaTag -Name $tag
+                            log 2 "Adding tag: $tag"
+                        }
+                    }
+                    $TroubleshootingData.PaDevice = $global:padeviceobject
+
+                    ###################################
+                    # Add Address Objects
+                    log 3 "Checking for $($DeviceList.Count) device(s)"
+                    foreach ($d in $DeviceList) {
+                        log 3 "$BaseMessage Looking up cpe: $d"
+                        $Lookup = $CurrentAddresses | Where-Object { ($_.Value -eq $d) -or ($_.Value -eq "$d/32") }
+                        if ($Lookup) {
+                            log 4 "$d`: found"
+                            if ($Lookup.Tags -notcontains $UserTag) {
+                                log 4 "$d`: adding tag: $UserTag"
+                                $Lookup | Set-PaAddress -Tag $UserTag
+                                $TroubleshootingData.PaDevice = $global:padeviceobject
+                            }
+                        } else {
+                            $Add = @{}
+                            $Add.Name = 'cpe_' + $d + '-32'
+                            $Add.Type = 'ip-netmask'
+                            $Add.Value = "$d/32"
+                            $Add.Tag = @('cpe', $UserTag)
+                            log 4 "Creating address: $($Add.Name)"
+                            $CreateAddress = Set-PaAddress @Add
+                            $TroubleshootingData.PaDevice = $global:padeviceobject
+                        }
+                    }
+
+                    ###################################
+                    # Add Destination Address Group
+                    $Filter = "'cpe' and '$UserTag'"
+                    $Lookup = Get-PaAddressGroup -Name $GroupName
+                    if ($Lookup) {
+                        log 4 "Address Group exists"
+                    } else {
+                        $Add = @{}
+                        $Add.Name = $GroupName
+                        $Add.Filter = $Filter
+                        log 4 "Creating Address Group: $($Add.Name)"
+                        $AddGroup = Set-PaAddressGroup @Add
+                    }
+
+                    ###################################
+                    # Add Create Security Policy
+
+                    $Lookup = Get-PaSecurityPolicy -Name $PolName
+                    if ($Lookup) {
+                        log 3 "Security Policy exists"
+                    } else {
+                        $Add = @{}
+                        $Add.Name = $PolName
+                        $Add.SourceZone = $Config.SourceZone
+                        $Add.DestinationZone = $Config.DestinationZone
+                        $Add.SourceUser = $User
+                        $Add.DestinationAddress = $GroupName
+                        $Add.Action = 'allow'
+
+                        log 4 "creating Security Policy $($Add.Name)"
+                        $AddRule = Set-PaSecurityPolicy @Add
+
+                        $Move = @{}
+                        $Move.Name = $PolName
+                        $Move.Top = $true
+
+                        log 4 "Moving Security Policy $($Add.Name)"
+                        $MoveRule = Move-PaSecurityPolicy @Move
+                    }
+                    break
+                }
+                'released' {
+                    log 3 "removing config for user $User"
+                    $RuleLookup = Get-PaSecurityPolicy -Name $PolName
+                    $GroupLookup = Get-PaAddressGroup -Name $GroupName
+                    $AddressLookup = $CurrentAddresses | Where-Object { $_.Tags -contains $UserTag }
+                    $TagLookup = Get-PaTag -Name $UserTag
+
+                    if ($RuleLookup) {
+                        log 4 "removing rule: $($RuleLookup.Name)"
+                        $RuleRemove = $RuleLookup | Remove-PaSecurityPolicy
+                    }
+
+                    if ($GroupLookup) {
+                        log 4 "removing address group: $($GroupLookup.Name)"
+                        $GroupRemove = $GroupLookup | Remove-PaAddressGroup
+                    }
+
+                    foreach ($address in $AddressLookup) {
+                        log 4 "removing address: $($address.Name)"
+                        $address | Remove-PaAddress
+                    }
+
+                    if ($TagLookup) {
+                        log 4 "removing tag: $($TagLookup.Name)"
+                        $TagRemove = $TagLookup | Remove-PaTag
+                    }
+                    break
+                }
+            }
+
+            if ($Config.Commit) {
+                log 1 "Commiting"
+                $Commit = Invoke-PaCommit -Wait
+            } else {
+                log 1 "Commit set to false, skipping Commit..."
+            }
+        } else {
+            log 1 "Invalid Post"
+            log 1 "Post Message: $Body"
+        }
+        $TroubleshootingData.PaDevice = $global:padeviceobject
+    } catch {
+        #TODO: needs some error handling
+        $TroubleshootingData.PaDevice = $global:padeviceobject
+        $TroubleshootingData.Error += $_
+    }
+
+    $StopWatch.Stop()
+    $ScriptSeconds = $StopWatch.Elapsed.Seconds
+    log 1 "Completed in $ScriptSeconds seconds"
+
+    $TroubleshootingData | Export-Clixml -Path $TsFile -Force
+}
+
+Start-UDRestApi -Endpoint $Endpoint -Port 10004 -Name $ApiName
